98da763a0cc31c69ba5790b51a46325f59a0cf70
Sarina Hemmat pour <hemmatpour.sarina@gmail.com>
2022-06-22 12:54:44 +0430

    scanner part added: the project is complete now

--- capture.py
+++ capture.py
@@ -1,3 +1,6 @@
+
+from sys import flags
+from telnetlib import IP
 from scapy.all import *
 
 
@@ -5,36 +8,74 @@ from scapy.all import *
 #a.summary()
 
 
-packets = rdpcap("C:/Users/almas/Documents/attack1.pcap")
+packets = rdpcap("C:/Users/Lenovo/Downloads/Compressed/trace4.pcap")
 length=len(packets)
 
 
-#all packets
+#show fields:
 #for i in packets:
- #   i.show()
+#    i.show()
+
+
+# packets[0].show()
 
 #the whole summary
 print(packets)
 
+
 #number off all packets
-print(f"Number of all packets: {length}")
+print(f"\nNumber of all packets: {length}")
 
 
-#percentageof tcp and udp packets
+#percentage of tcp and udp packets
 countTCP=0
 countUDP=0
 
 for i in range(0, len(packets)):
-          pkt = packets[i]
-          if (TCP in pkt):
-                   countTCP+= 1
-          elif (UDP in pkt):
-                   countUDP+= 1
+    pkt = packets[i]
+    if (TCP in pkt):
+        countTCP+= 1
+    elif (UDP in pkt):
+        countUDP+= 1
 
-print(f"TCP packets= {(countTCP/length)*100}%")
+print(f"\nTCP packets= {(countTCP/length)*100}%")
 print(f"UDP packets= {(countUDP/length)*100}%")
 
-#number of all fragments ******************************
+#number of fragmented datagrams: **
+
+print("\n**Number of fragmented datagrams:**")
+
+DFs = 0
+for i in packets[IP]:
+    if i.flags==2: #DF  010 i.flags=="DF"
+        DFs+=1
+
+fragmented = length-DFs
+
+print(f"\nNumber of fragmented datagrams= {fragmented}")
+
+#scanning **
+srcIP=input('\n\n146.137.96.1Enter the source IP:  ')
+
+# srcS=[]
+
+countS=0
+countSA = 0
+
+for i in packets[IP]:
+    if i.src == srcIP:
+        if i.flags == 18:
+            countSA +=1
+
+        if i.flags == 2:
+            countS +=1
 
 
+print(f"\nSYN/ACK: {countSA}")
+print(f"SYN: {countS}")
 
+if countSA!=0:
+    if countS/countSA>3:
+        print(f"\nWARNING _ scanning attack !!! {countS/countSA>3}")
+else: 
+    print('\n no attack detected')
